ce302d3be47ccb76036396ce5144bfb1aa5c9b05
David Alvarado <36020778+SailorDave17@users.noreply.github.com>
2021-08-25 17:23:52 -0400

    simplified functions

---
+++ src/com/basics/FunctionsSimplified.java
@@ -0,0 +1,35 @@
+package com.basics;
+
+public class FunctionsSimplified {
+
+    public static void main(String[] args) {
+        printName();
+        int numA = 5;
+        int numB = 2;
+        addNumbers(numA, numB);
+        multiplyNumbers(numA, numB);
+
+
+
+    }
+
+    static void printName(){
+        String name = "Dave";
+        System.out.println("My name is " + name);
+    }
+
+    static void addNumbers(int numberA, int numberB){
+       int sum = numberA + numberB;
+        System.out.println(sum);
+
+    }
+
+    static int multiplyNumbers(int valueA, int valueB){
+        int product = valueA * valueB;
+        addNumbers(product, product);
+        return product;
+
+    }
+
+
+}
