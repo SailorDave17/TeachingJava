cc834c67ae34955a713f4a95ad3e912af83d2c87
David Alvarado <36020778+SailorDave17@users.noreply.github.com>
2021-08-25 14:08:39 -0400

    If-then weather program

--- src/com/basics/Weather.java
+++ src/com/basics/Weather.java
@@ -5,17 +5,20 @@ public class Weather {
         // This program will give us suggestions of what to wear based on the weather(temperature)
 
         int temperature = 20;
-        String sunCondition = "Sunny";
+        int sunCondition = 90;
 
         if (temperature > 80) {
             System.out.println("It's Nice out, wear shorts and a T-Shirt");
-        } else if (temperature > 60) {
+        } else if ((temperature > 60) && (sunCondition > 80)) {
             System.out.println("It's a little cooler. Wear jeans and a long sleeve shirt");
+            System.out.println("Wear sunglasses as well");
 
+        } else if ((temperature > 50) || (sunCondition < 70)) {
+            System.out.println("This a cool, overcast day, wear warm clothes");
         } else {
             System.out.println("It's pretty cold, bring a jacket");
 
-            //test
+
         }
     }
 }
