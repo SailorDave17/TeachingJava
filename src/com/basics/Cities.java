c2408fd4bee87d84e7a66221362249dbb94341df
David Alvarado <36020778+SailorDave17@users.noreply.github.com>
2021-08-25 18:21:38 -0400

    while loop

--- src/com/basics/Cities.java
+++ src/com/basics/Cities.java
@@ -2,12 +2,25 @@ package com.basics;
 
 public class Cities {
     public static void main(String[] args) {
+
         //Declare & define the array
         String[] cities = {"New York", "Columbus", "Chicago", "Ft. Lauderdale"};
         System.out.println(cities[0]);
         System.out.println(cities[1]);
         System.out.println(cities[2]);
         System.out.println(cities[3]);
+
+        //Declare the array
+        String[] countries;
+
+        //Define the array
+        countries = new String[3];
+        countries[0] = "United States";
+        countries[1]= "Canada";
+        countries[2]= "Mexico";
+        System.out.println(countries[2]);
+
+        System.out.println("---------------------------------------");
 
         //Declare & define the array (only size)
         String[] states = new String[5];
@@ -16,20 +29,43 @@ public class Cities {
         states[2] = "Alaska";
         states[3] = "Florida";
         states[4] = "Michigan";
-        System.out.println(states[0]);
+
+        //how do we do this using loops?
+
+//        System.out.println(states[0]);
+//        System.out.println(states[1]);
+//        System.out.println(states[2]);
+//        System.out.println(states[3]);
+//        System.out.println(states[4]);
+
+
+        int i = 0;
+
+        //Do Loop: Enters the loop, THEN tests the condition
+        do{
+            System.out.println("State: " + states[i]);
+            i++;
+        }while (i<5);
+
+//        While Loop: tests condition FIRST, then enters the loop
+        int n= 0;
+        boolean stateFound = false;
+        while (!stateFound){
+            String state = states[n];
+            System.out.println(state);
+             if(state == "Florida"){
+                 System.out.println("STATE FOUND");
+                 stateFound = true;
+
+             }
+             n++;
+
+        }
+
+        //For Loop: Best structure for iterating through an array
 
 
 
-        //Declare the array
-        String[] countries;
-
-
-        //Define the array
-        countries = new String[3];
-        countries[0] = "United States";
-        countries[1]= "Canada";
-        countries[2]= "Mexico";
-        System.out.println(countries[2]);
 
 
     }
