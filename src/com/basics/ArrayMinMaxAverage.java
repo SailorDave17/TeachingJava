87edf3aebff22100ad95897b0b5903678232a251
David Alvarado <36020778+SailorDave17@users.noreply.github.com>
2021-08-28 12:45:41 -0400

    labs

---
+++ src/com/basics/ArrayMinMaxAverage.java
@@ -0,0 +1,50 @@
+package com.basics;
+
+public class ArrayMinMaxAverage {
+    public static void main(String[] args) {
+        int myArray[] = new int[]{2,1,6,8,9,4};
+        int max = getMax(myArray);
+        int min = getMin(myArray);
+        int ave = getAve(myArray);
+
+        System.out.println("The minimum value of the Array is " + min);
+        System.out.println("The average value of the Array is " + ave);
+        System.out.println("The maximum value of the Array is " + max);
+
+    }
+
+    public static int getMin(int[]arrayInput){
+        int minValue = arrayInput[0];
+        for(int i =1; i<arrayInput.length; i++){
+            if(arrayInput[i] < minValue){
+                minValue = arrayInput[i];
+            }
+        }
+        return minValue;
+
+    }
+
+    public static int getMax(int[]arrayInput){
+        int maxValue = arrayInput[0];
+        for (int i=1 ; i < arrayInput.length; i++){
+            if(arrayInput[i]>maxValue){
+                maxValue = arrayInput[i];
+            }
+        }
+        return maxValue;
+    }
+
+    public static int getAve(int[]arrayInput){
+        int aveValue;
+        int totalValue = 0;
+        for (int i = 0; i<arrayInput.length; i++){
+            totalValue = totalValue + arrayInput[i];
+
+        }
+
+        aveValue = totalValue/arrayInput.length;
+        return aveValue;
+    }
+
+
+}
---
+++ src/com/basics/SumofNumbersOneToN.java
@@ -0,0 +1,11 @@
+package com.basics;
+
+public class SumofNumbersOneToN {
+    public static void main(String[] args) {
+
+        int n = 4;
+        System.out.println(n*(n+1)/2);
+
+    }
+
+}
